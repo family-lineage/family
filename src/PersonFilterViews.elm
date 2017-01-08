ecf7e3f32ef94d310081315b70b31548cb753281
Aldiantoro Nugroho <kriwil@gmail.com>
2017-01-08 19:27:56 +0700

    add father/mother/spouse button from filtered people

--- src/PersonFilterViews.elm
+++ src/PersonFilterViews.elm
@@ -1,7 +1,12 @@
 module PersonFilterViews exposing (filterView)
 
-import Html exposing (Html, div, input, text)
-import Html.Events exposing (onInput)
+import Html exposing (Html, div, input, text, span)
+import Html.Events exposing (onInput, onClick)
+import Material.Textfield as Textfield
+import Material.Button as Button
+import Material.List as List
+import Material.Elevation as Elevation
+import Material.Options as Options
 import Messages exposing (Msg(..))
 import Models exposing (Model, People, Person, PersonId)
 
@@ -14,27 +19,76 @@ filteredPeople name people =
                 |> String.trim
                 |> String.toLower
     in
-        if String.length cleanName > 1 then
+        if String.length cleanName > 0 then
             List.filter
-                (\person -> String.contains cleanName (String.toLower person.name))
+                (\person ->
+                    person.name
+                        |> String.toLower
+                        |> String.contains cleanName
+                )
                 people
         else
-            []
+            people
 
 
-personView : Person -> Html Msg
-personView person =
-    div [] [ text person.name ]
+addButton : Model -> List Int -> String -> Msg -> Html Msg
+addButton model indexes labelText msg =
+    Button.render Mdl
+        indexes
+        model.mdl
+        [ Button.ripple, Options.onClick msg ]
+        [ text labelText ]
+
+
+nameField : Model -> Html Msg
+nameField model =
+    Textfield.render
+        Mdl
+        [ 9, 0 ]
+        model.mdl
+        [ Textfield.label "Type to search"
+        , Textfield.text_
+        , Textfield.value model.filterPersonName
+        , Options.onInput ChangeFilterPersonName
+        ]
+        []
+
+
+personView : Model -> Person -> Html Msg
+personView model person =
+    let
+        itemOptions =
+            if Just person.id == model.personId then
+                [ Elevation.e2 ]
+            else
+                []
+
+        familyButtons =
+            if Just person.id == model.personId then
+                []
+            else
+                [ addButton model [ 9 + person.id, 0 ] "+Father" (ChangePersonFather (Just person.id))
+                , addButton model [ 9 + person.id, 1 ] "+Mother" (ChangePersonMother (Just person.id))
+                , addButton model [ 9 + person.id, 2 ] "+Spouse" (ChangePersonSpouse (Just person.id))
+                ]
+    in
+        List.li itemOptions
+            ([ List.content
+                [ Options.attribute <| onClick (UpdatePerson person) ]
+                [ text person.name ]
+             ]
+                ++ familyButtons
+            )
 
 
 filterView : Model -> Html Msg
 filterView model =
     div []
         [ div []
-            [ input [ onInput ChangeFilterPersonName ] [] ]
-        , div []
+            [ nameField model ]
+        , List.ul []
             (model.people
                 |> filteredPeople model.filterPersonName
-                |> List.map personView
+                |> List.map (personView model)
             )
         ]
--- src/Views.elm
+++ src/Views.elm
@@ -2,7 +2,7 @@ module Views exposing (..)
 
 import Html exposing (Html, div, text, label, h2, p, hr, span)
 import Html.Attributes exposing (style)
-import Html.Events exposing (onSubmit)
+import Html.Events exposing (onSubmit, onClick)
 import Material.Grid exposing (grid, cell, size, Device(..))
 import Material.Layout as Layout
 import Material.Textfield as Textfield
@@ -62,26 +62,26 @@ personForm model =
                 [ checkbox ToggleIsPersonSelf model "Is this you?" ]
             , cell row
                 [ label []
-                    [ text
-                        ("Father "
-                            ++ getPersonName model.people model.personFather
-                        )
+                    [ text "Father: "
+                    , span
+                        [ onClick (ChangePersonFather Nothing) ]
+                        [ text (getPersonName model.people model.personFather) ]
                     ]
                 ]
             , cell row
                 [ label []
-                    [ text
-                        ("Mother "
-                            ++ getPersonName model.people model.personMother
-                        )
+                    [ text "Mother: "
+                    , span
+                        [ onClick (ChangePersonMother Nothing) ]
+                        [ text (getPersonName model.people model.personMother) ]
                     ]
                 ]
             , cell row
                 [ label []
-                    [ text
-                        ("Spouse"
-                            ++ getPersonName model.people model.personSpouse
-                        )
+                    [ text "Spouse: "
+                    , span
+                        [ onClick (ChangePersonSpouse Nothing) ]
+                        [ text (getPersonName model.people model.personSpouse) ]
                     ]
                 ]
             , cell row
